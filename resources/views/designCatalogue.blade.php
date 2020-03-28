ee6540d8b0fb7723f2e99b365c0a8a91e3c7cd16
FriendlyVideogamePlayer <zakariahahah@gmail.com>
2020-03-28 12:11:56 +0000

    Initial commit of project
    Base upload of laravel.

---
+++ resources/views/designCatalogue.blade.php
@@ -0,0 +1,90 @@
+<html>
+@extends('layout.head')
+</html>
+
+<body>
+
+<div class="container-fluid">
+  <div class="row">
+    <div class="col-sm">
+        
+    <div class="card" style="width: 22rem;">
+    <img class="card-img-top" src="https://www.gettyimages.pt/gi-resources/images/Homepage/Hero/PT/PT_hero_42_153645159.jpg" alt="Card image cap">
+    <div class="card-body">
+        <h5 class="card-title">Username</h5>
+        <p class="card-text">Some quick example text to simulate a description.</p>
+    </div>
+    </div>
+
+    </div>
+    <div class="col-sm">
+     
+    <div class="card" style="width: 22rem;">
+    <img class="card-img-top" src="https://www.gettyimages.pt/gi-resources/images/Homepage/Hero/PT/PT_hero_42_153645159.jpg" alt="Card image cap">
+    <div class="card-body">
+        <h5 class="card-title">Username</h5>
+        <p class="card-text">Some quick example text to simulate a description.</p>
+    </div>
+    </div>
+
+    </div>
+    <div class="col-sm">
+     
+    <div class="card" style="width: 22rem;">
+    <img class="card-img-top" src="https://www.gettyimages.pt/gi-resources/images/Homepage/Hero/PT/PT_hero_42_153645159.jpg" alt="Card image cap">
+    <div class="card-body">
+        <h5 class="card-title">Username</h5>
+        <p class="card-text">Some quick example text to simulate a description.</p>
+    </div>
+    </div>
+
+    </div>
+
+    <div class="col-sm">
+     
+    <div class="card" style="width: 22rem;">
+    <img class="card-img-top" src="https://www.gettyimages.pt/gi-resources/images/Homepage/Hero/PT/PT_hero_42_153645159.jpg" alt="Card image cap">
+    <div class="card-body">
+        <h5 class="card-title">Username</h5>
+        <p class="card-text">Some quick example text to simulate a description.</p>
+    </div>
+    </div>
+
+    </div>
+
+    <div class="col-sm">
+     
+    <div class="card" style="width: 22rem;">
+    <img class="card-img-top" src="https://www.gettyimages.pt/gi-resources/images/Homepage/Hero/PT/PT_hero_42_153645159.jpg" alt="Card image cap">
+    <div class="card-body">
+        <h5 class="card-title">Username</h5>
+        <p class="card-text">Some quick example text to simulate a description.</p>
+    </div>
+    </div>
+
+    </div>
+
+    <div class="col-sm">
+     
+    <div class="card" style="width: 22rem;">
+    <img class="card-img-top" src="https://www.gettyimages.pt/gi-resources/images/Homepage/Hero/PT/PT_hero_42_153645159.jpg" alt="Card image cap">
+    <div class="card-body">
+        <h5 class="card-title">Username</h5>
+        <p class="card-text">Some quick example text to simulate a description.</p>
+    </div>
+    </div>
+
+    </div>
+
+  </div>
+</div>
+
+
+
+    
+
+
+
+
+
+</body>
