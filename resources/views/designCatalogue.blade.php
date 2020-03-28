896acb8836f28241ee61758ccdc9c8e5089a44c7
FriendlyVideogamePlayer <zakariahahah@gmail.com>
2020-03-28 13:52:15 +0000

    Add nav, footer and base design of main page
    -Added a simple footer
    -Added a simple nav
    -Added a card layout view to the main designs page

--- resources/views/designCatalogue.blade.php
+++ resources/views/designCatalogue.blade.php
@@ -1,15 +1,16 @@
 <html>
 @extends('layout.head')
+@include('layout.nav')
 </html>
 
 <body>
 
 <div class="container-fluid">
   <div class="row">
-    <div class="col-sm">
+    <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3 px-5 py-5">
         
-    <div class="card" style="width: 22rem;">
-    <img class="card-img-top" src="https://www.gettyimages.pt/gi-resources/images/Homepage/Hero/PT/PT_hero_42_153645159.jpg" alt="Card image cap">
+    <div class="card" >
+    <img class="card-img-top" src="https://i.redd.it/yx5s9sib1bp41.jpg" alt="Card image cap">
     <div class="card-body">
         <h5 class="card-title">Username</h5>
         <p class="card-text">Some quick example text to simulate a description.</p>
@@ -17,10 +18,10 @@
     </div>
 
     </div>
-    <div class="col-sm">
+    <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3 px-5 py-5">
      
-    <div class="card" style="width: 22rem;">
-    <img class="card-img-top" src="https://www.gettyimages.pt/gi-resources/images/Homepage/Hero/PT/PT_hero_42_153645159.jpg" alt="Card image cap">
+    <div class="card" >
+    <img class="card-img-top" src="https://i.redd.it/yx5s9sib1bp41.jpg" alt="Card image cap">
     <div class="card-body">
         <h5 class="card-title">Username</h5>
         <p class="card-text">Some quick example text to simulate a description.</p>
@@ -28,22 +29,10 @@
     </div>
 
     </div>
-    <div class="col-sm">
+    <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3 px-5 py-5">
      
-    <div class="card" style="width: 22rem;">
-    <img class="card-img-top" src="https://www.gettyimages.pt/gi-resources/images/Homepage/Hero/PT/PT_hero_42_153645159.jpg" alt="Card image cap">
-    <div class="card-body">
-        <h5 class="card-title">Username</h5>
-        <p class="card-text">Some quick example text to simulate a description.</p>
-    </div>
-    </div>
-
-    </div>
-
-    <div class="col-sm">
-     
-    <div class="card" style="width: 22rem;">
-    <img class="card-img-top" src="https://www.gettyimages.pt/gi-resources/images/Homepage/Hero/PT/PT_hero_42_153645159.jpg" alt="Card image cap">
+    <div class="card" >
+    <img class="card-img-top" src="https://i.redd.it/yx5s9sib1bp41.jpg" alt="Card image cap">
     <div class="card-body">
         <h5 class="card-title">Username</h5>
         <p class="card-text">Some quick example text to simulate a description.</p>
@@ -52,10 +41,10 @@
 
     </div>
 
-    <div class="col-sm">
+    <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3 px-5 py-5">
      
-    <div class="card" style="width: 22rem;">
-    <img class="card-img-top" src="https://www.gettyimages.pt/gi-resources/images/Homepage/Hero/PT/PT_hero_42_153645159.jpg" alt="Card image cap">
+    <div class="card" >
+    <img class="card-img-top" src="https://i.redd.it/yx5s9sib1bp41.jpg" alt="Card image cap">
     <div class="card-body">
         <h5 class="card-title">Username</h5>
         <p class="card-text">Some quick example text to simulate a description.</p>
@@ -64,10 +53,22 @@
 
     </div>
 
-    <div class="col-sm">
+    <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3 px-5 py-5">
      
-    <div class="card" style="width: 22rem;">
-    <img class="card-img-top" src="https://www.gettyimages.pt/gi-resources/images/Homepage/Hero/PT/PT_hero_42_153645159.jpg" alt="Card image cap">
+    <div class="card" >
+    <img class="card-img-top" src="https://i.redd.it/yx5s9sib1bp41.jpg" alt="Card image cap">
+    <div class="card-body">
+        <h5 class="card-title">Username</h5>
+        <p class="card-text">Some quick example text to simulate a description.</p>
+    </div>
+    </div>
+
+    </div>
+
+    <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3 px-5 py-5">
+     
+    <div class="card" >
+    <img class="card-img-top" src="https://i.redd.it/yx5s9sib1bp41.jpg" alt="Card image cap">
     <div class="card-body">
         <h5 class="card-title">Username</h5>
         <p class="card-text">Some quick example text to simulate a description.</p>
@@ -86,5 +87,5 @@
 
 
 
-
+@include('layout.footer')
 </body>
