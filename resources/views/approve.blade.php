c000a6d5950e72cd7c576679674d33abd6fbcc15
FriendlyVideogamePlayer <zakariahahah@gmail.com>
2020-03-30 09:48:50 +0100

    Add approval system
    -Approve page exists where designs can be approved
    -Adds to designs table and removes from uploads

--- resources/views/approve.blade.php
+++ resources/views/approve.blade.php
@@ -6,13 +6,18 @@
 <body>
 
 <div class="container">
+    @isset($successMessage)
+        <div class="alert alert-success" role="alert" style="text-align:center;">
+            {{$successMessage}}
+        </div>
+    @endisset
 
     @if(count($designs) > 0)
     <div class="card-deck">
         @foreach($designs as $design)
             <div class="card mb-4" style="min-width: 20rem;">
                 <a href="http://161.35.38.150/designs/{{$design->id}}" class="cardLink cardHover">
-                    <img class="card-img-top" src="https://i.redd.it/yx5s9sib1bp41.jpg" alt="Card image cap">
+                    <img class="card-img-top" src="{{$design->imagelink}}" alt="Card image cap">
                     <div class="card-body">
                         <h5 class="card-title">{{$design->title}}</h5>
                         <p class="card-text">{{$design->description}}</p>
@@ -23,7 +28,16 @@
                     </div>
                 </a>
                 <div class="card-body">
-                    <button type="submit" class="btn btn-primary">Approve!</button>
+                    <form action="{{ route('approve.designs') }}" method="post">
+                        {{ csrf_field() }}
+                        <input type="hidden" name="id" value="{{ $design->id }}">
+                        <input type="hidden" name="title" value="{{ $design->title }}">
+                        <input type="hidden" name="description" value="{{ $design->description }}">
+                        <input type="hidden" name="username" value="{{ $design->username }}">
+                        <input type="hidden" name="designType" value="{{ $design->designtype }}">
+                        <input type="hidden" name="imageLink" value="{{ $design->imagelink }}">
+                        <button type="submit" class="btn btn-primary">Approve!</button>
+                    </form>
                 </div>
             </div>
         @endforeach
