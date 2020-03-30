ed54148254125a1171a1a12c2c81e7a2d38f065d
FriendlyVideogamePlayer <zakariahahah@gmail.com>
2020-03-30 10:16:16 +0100

    Change filter form display
    -Make search button appear on same line on bigger displays
    -Adjust widths of each element
    -Make filter slightly bigger (60%)

--- resources/views/layout/designFilter.blade.php
+++ resources/views/layout/designFilter.blade.php
@@ -2,11 +2,11 @@
     <form id="uploadInput" class="formStyle" action="http://161.35.38.150/search" method="post">
     @csrf
         <div class="row"> 
-            <div class="col-xs-12 col-md-6 py-3">
+            <div class="col-xs-12 col-md-5 py-3">
                     <label for="filterInput">Search for a design</label>
                     <input type="text" class="form-control" id="filterInput" name="filterInput" placeholder="">
             </div>
-            <div class="col-xs-12 col-md-6 py-3">
+            <div class="col-xs-12 col-md-5 py-3">
                     <label for="typeSelect">Item type</label>
                     <select name="filterSelect" class="form-control" id="typeSelect">
                         <option value="All">Choose a type</option>
@@ -17,9 +17,10 @@
                         <option value="Room Design">Room Design</option>
                     </select>
             </div>
-        </div>
-        <div class="col py-3">
-            <button type="submit" class="btn btn-primary">Search!</button>
+        
+            <div class="col-xs-12 col-md-2 pb-2 pt-4 mt-4">
+                <button type="submit" class="btn btn-primary">Search!</button>
+            </div>
         </div>
     </form>
 </div>
